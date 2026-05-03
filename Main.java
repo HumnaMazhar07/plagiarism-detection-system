4e1307e3a7aa30a6d0f44112a4ff5923693d39bd
Humna Mazhar <humnamazharvii@gmail.com>
2026-05-04 02:30:16 +0500

    Add Main class with StudentDashboard initialization

---
+++ Main.java
@@ -0,0 +1,9 @@
+package application;
+
+public class Main {
+    public static AssignmentQueue queue = new AssignmentQueue();
+
+    public static void main(String[] args) {
+        new StudentDashboard().setVisible(true);
+    }
+}
