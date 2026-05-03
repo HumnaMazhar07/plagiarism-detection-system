2520db7356ed0b1acda410ba9660b6aaffe5da76
Humna Mazhar <humnamazharvii@gmail.com>
2026-05-04 02:25:48 +0500

    Add AssignmentNode class for linked list structure

---
+++ AssignmentNode.java
@@ -0,0 +1,12 @@
+package application;
+
+public class AssignmentNode {
+	Assignment data;
+    AssignmentNode next;
+
+    AssignmentNode(Assignment a) {
+        data = a;
+        next = null;
+    }
+
+}
